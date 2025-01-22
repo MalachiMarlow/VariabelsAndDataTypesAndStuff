17ff3fea5fcebe424e773c74b7e2920673042dd0
Tusk <Tuskman045@gmail.com>
2025-01-22 08:52:52 -0700

    Added Header & jellyBeans code.

---
+++ VariabelsAndDataTypesAndStuff/VariabelsAndDataTypesAndStuff.vb
@@ -0,0 +1,28 @@
+﻿'Malachi Marlow
+
+Option Explicit On
+
+Module VariabelsAndDataTypesAndStuff
+
+    Sub Main()
+
+        Dim jellyBeans As Integer
+        Dim LaffyTaffy As Integer
+
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = 120
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = jellyBeans + 10
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans += 10
+        Console.WriteLine(jellyBeans)
+
+        LaffyTaffy = 62
+        Console.WriteLine(LaffyTaffy)
+
+    End Sub
+
+End Module
